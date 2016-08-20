f09af6ca2e7d4aadbe0e9711369c35bda72d1718
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-08-20 16:32:50 -0700

    finished countBits

--- counting-bits.playground/Contents.swift
+++ counting-bits.playground/Contents.swift
@@ -1,2 +1,34 @@
 import Foundation
 
+class Solution {
+    func countBits(num: Int) -> [Int] {
+        var nums = [Int](count: num+1, repeatedValue: 0)
+        if num == 0 { // if num is 0
+            return nums
+        }
+        for i in 1...num {
+            nums[i] = binaryOnes(i)
+        }
+        return nums
+    }
+    func binaryOnes(num: Int) -> Int {
+        var num = num
+        var count = 0
+        repeat  {
+            if num % 2 == 0 { // even numbers
+                num /= 2
+            } else { // all odd numbers
+                num = (num-1)/2
+                count += 1
+            }
+        } while num > 1
+        if num == 1 { // when num is 1 at the end
+            count += 1
+        }
+        return count
+    }
+}
+
+let solution = Solution()
+solution.countBits(0)
+
