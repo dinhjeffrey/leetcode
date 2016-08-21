491941ccdc3b641fb12bfc2e7c315a9823da1331
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-08-20 18:28:03 -0700

    added single number

--- single-number.playground/Contents.swift
+++ single-number.playground/Contents.swift
@@ -1,1 +1,16 @@
 import Foundation
+class Solution {
+    // MARK: - A ^ A ^ B = 0 ^ B = B
+    // xor subtracts if number was already in result, else it adds to result (uses binary values)
+    func singleNumber(nums: [Int]) -> Int {
+        var result = 0
+        for num in nums {
+            result = num ^ result
+        }
+        return result
+    }
+}
+
+let solution = Solution()
+solution.singleNumber([555, 2,3,2,3,1, 555])
+
