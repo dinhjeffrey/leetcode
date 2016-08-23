435e21405bf0029c2b314b052e05290be7c1c992
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-08-22 18:20:04 -0700

    solved in O^n 🐶

--- single-number-III.playground/Contents.swift
+++ single-number-III.playground/Contents.swift
@@ -3,21 +3,33 @@ import Foundation
 class Solution {
     func singleNumber(nums: [Int]) -> [Int] {
         var result = 0
+        // store xor in result
         for num in nums {
-            result = num ^ result
+            result ^= num
         }
-        for index1 in 0..<nums.count-1 {
-            for index2 in index1+1..<nums.count {
-                let xor = nums[index1] ^ nums[index2]
-                switch xor {
-                case let x where x == result:
-                    return [nums[index1], nums[index2]]
-                default: // x < result
-                    break
-                }
+        // Get its last set bit
+        result &= -result
+        var results = [Int](count: 2, repeatedValue: 0)
+        for num in nums {
+            if num & result == 0 { // the bit is not set
+                results[0] ^= num
+            } else { // bit is set
+                results[1] ^= num
             }
         }
-        return [0] // never reaches here
+        return results
+//        for index1 in 0..<nums.count-1 {
+//            for index2 in index1+1..<nums.count {
+//                let xor = nums[index1] ^ nums[index2]
+//                switch xor {
+//                case let x where x == result:
+//                    return [nums[index1], nums[index2]]
+//                default: // x < result
+//                    break
+//                }
+//            }
+//        }
+//        return [0] // never reaches here
     }
 }
 
